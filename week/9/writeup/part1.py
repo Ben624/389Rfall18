d89da5a8da62096fe35d424bc0d97f7fa8436b42
Ben624 <beisner@terpmail.umd.edu>
2018-11-11 10:43:09 -0500

    Finished part1.py

---
+++ week/9/writeup/part1.py
@@ -0,0 +1,24 @@
+
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+# importing a useful library -- feel free to add any others you find necessary
+import hashlib
+import string
+
+# this will work if you place this script in your writeup folder
+wordlist = open("../probable-v2-top1575.txt", 'r')
+
+# a string equal to 'abcdefghijklmnopqrstuvwxyz'.
+salts = string.ascii_lowercase
+
+# a set of the hashet split with the new line character
+hashSet = set(open('hashes', 'r').read().split('\n'))
+
+for word in wordlist:
+    for salt in salts:
+    # do stuff
+        word = word.strip()
+        hsh = hashlib.sha512(salt+word).hexdigest()
+        if(hsh in hashSet):
+            print 'Salt:%s  Password:%s' % (salt, word)
